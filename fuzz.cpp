6e17b70c90e8fcd3d4bcec760330448f6a7ebc03
Ethan Graf <graf.ethan@gmail.com>
2022-02-28 18:31:25 -0500

    Update fuzz.cpp

--- fuzz.cpp
+++ fuzz.cpp
@@ -18,33 +18,3 @@ void FuzzBrokenMethod3(std::string data) {
 void FuzzBrokenMethod4(std::string data) {
     BrokenMethod2(data);
 }
-
-
-void FuzzBrokenMethod5(std::string data) {
-    BrokenMethod2(data);
-}
-
-
-void FuzzBrokenMethod6(std::string data) {
-    BrokenMethod2(data);
-}
-
-
-void FuzzBrokenMethod7(std::string data) {
-    BrokenMethod2(data);
-}
-
-
-void FuzzBrokenMethod8(std::string data) {
-    BrokenMethod2(data);
-}
-
-
-void FuzzBrokenMethod9(std::string data) {
-    BrokenMethod2(data);
-}
-
-
-void FuzzBrokenMethod10(std::string data) {
-    BrokenMethod2(data);
-}
