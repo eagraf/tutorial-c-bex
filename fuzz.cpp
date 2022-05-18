caf509018bed71acc5252ca799b0c7e30b07a9c3
Ethan Graf <graf.ethan@gmail.com>
2022-05-18 00:08:11 -0400

    Update fuzz.cpp

--- fuzz.cpp
+++ fuzz.cpp
@@ -1,12 +1,12 @@
 #include "api.h"
 
-/*void FuzzBrokenMethod(std::string data) {
+void FuzzBrokenMethod(std::string data) {
     BrokenMethod2(data);
 }
 
 void FuzzBrokenMethod2(std::string data) {
     BrokenMethod2(data);
-}*/
+}
 
 
 /*void FuzzBrokenMethod3(std::string data) {
